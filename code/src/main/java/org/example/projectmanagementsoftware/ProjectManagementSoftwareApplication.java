648f241144556917d5903e65444f3dd3f95df3fa
Andrey <petrodrovosek@201005@gmail.com>
2025-11-22 12:16:45 +0200

    second

---
+++ code/src/main/java/org/example/projectmanagementsoftware/ProjectManagementSoftwareApplication.java
@@ -0,0 +1,13 @@
+package org.example.projectmanagementsoftware;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjectManagementSoftwareApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run (ProjectManagementSoftwareApplication.class, args);
+    }
+
+}
